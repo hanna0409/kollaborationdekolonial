08b42a7cb1f7b67114ca53382421cc9a92645aea
tim4497 <tim.winkler@campus.tu-berlin.de>
2021-07-27 23:20:12 +0200

    umstrukturiert und die Map ist fertig

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+
+var bounds = [[0,0], [1000,1000]];
+
+var map = L.map('map', {
+    crs: L.CRS.Simple,
+    minZoom: 0,
+    maxZoom: 2,
+    maxBounds: bounds,
+    zoomDelta: 0.33,
+    zoomSnap: 0.3,
+    wheelPxPerZoomLevel: 100,
+    maxBoundsViscosity: 0.75
+});
+
+var image = L.imageOverlay(
+    './assets/bilder/WebsiteLayoutFINAL2.png', 
+    bounds
+    ).addTo(map);
+
+map.fitBounds(bounds);
+
+map.setView([500, 500], 1.5)
+
+var lilaMaker = L.icon({
+    iconUrl: './assets/marker/Marker_lila.png',
+    iconSize: [100, 100]
+});
+
+
+L.marker([500, 500], {
+    icon: lilaMaker 
+}).addTo(map).bindPopup('TestTest').openPopup();;
